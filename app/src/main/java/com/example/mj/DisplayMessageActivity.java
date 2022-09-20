b584b4a2ef9eb7ffcdb99ba801041a8c95d7b45b
MDog37127 <bentleymdog@gmail.com>
2022-09-19 22:13:48 -0500

    did the things

--- app/src/main/java/com/example/mj/DisplayMessageActivity.java
+++ app/src/main/java/com/example/mj/DisplayMessageActivity.java
@@ -15,9 +15,30 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message2 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+        String message3 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE3);
+        String message4 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE4);
+        String message5 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE5);
+        String message6 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE6);
+        String message7 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE7);
+        String message8 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE8);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.nounspace);
         textView.setText(message);
+        TextView textView2 = findViewById(R.id.verbspace);
+        textView2.setText(message);
+        TextView textView3 = findViewById(R.id.adverbspace);
+        textView3.setText(message);
+        TextView textView4 = findViewById(R.id.adjectivespace);
+        textView4.setText(message);
+        TextView textView5 = findViewById(R.id.random3space);
+        textView5.setText(message);
+        TextView textView6 = findViewById(R.id.randomspace);
+        textView6.setText(message);
+        TextView textView7 = findViewById(R.id.random2space);
+        textView7.setText(message);
+        TextView textView8 = findViewById(R.id.random4space);
+        textView8.setText(message);
     }
 }
--- app/src/main/java/com/example/mj/MainActivity.java
+++ app/src/main/java/com/example/mj/MainActivity.java
@@ -9,6 +9,14 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.mj.MESSAGE";
+    public static final String EXTRA_MESSAGE2 = "com.example.mj.MESSAGE2";
+    public static final String EXTRA_MESSAGE3 = "com.example.mj.MESSAGE3";
+    public static final String EXTRA_MESSAGE4 = "com.example.mj.MESSAGE4";
+    public static final String EXTRA_MESSAGE5 = "com.example.mj.MESSAGE5";
+    public static final String EXTRA_MESSAGE6 = "com.example.mj.MESSAGE6";
+    public static final String EXTRA_MESSAGE7 = "com.example.mj.MESSAGE7";
+    public static final String EXTRA_MESSAGE8 = "com.example.mj.MESSAGE8";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +26,32 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText editText = findViewById(R.id.noun);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        /**
+        EditText editText2 = findViewById(R.id.random);
+        String message2 = editText2.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE2, message2);
+        EditText editText3 =  findViewById(R.id.adverb);
+        String message3 = editText3.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE3, message3);
+        EditText editText4 =  findViewById(R.id.adjective);
+        String message4 = editText4.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE4, message4);
+        EditText editText5 =  findViewById(R.id.random3);
+        String message5 = editText5.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE5, message5);
+        EditText editText6 = findViewById(R.id.verb);
+        String message6 = editText6.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE6, message6);
+        EditText editText7 = findViewById(R.id.random2);
+        String message7 = editText7.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE7, message7);
+        EditText editText8 = findViewById(R.id.random4);
+        String message8 = editText8.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE8, message8);
+         **/
         startActivity(intent);
     }
     public void sendForAdventure(View view) {
